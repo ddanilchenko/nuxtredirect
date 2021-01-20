e8edac6460d1d95d8740f216eeec79c599e23911
Dmitry Danilchenko <ddanilchenko@dataxdev.com>
2021-01-20 16:48:10 +0200

    remove dirty hack for ///

--- middleware/noroute.js
+++ middleware/noroute.js
@@ -1,17 +1,5 @@
 export default function ({ redirect, route }) {
-    //console.log('noroute==', route)
     if (!route.name) {
-  
-      //dirty hack for redirect from root doc with multiple trailing slashes to single trailing slash 
-      const moreTanTwoRootTrailingSlashesRegex = new RegExp('^(\/)\{2,\}\$', 'g')
-      const moreThanTwoTrailingSlashes = moreTanTwoRootTrailingSlashesRegex.test(route.path)
-      
-      if (moreThanTwoTrailingSlashes) {
-        console.log('root file name redirect goes here:', route.path)
-        redirect(302, "/")
-        return
-      }
-      
       redirect(404, "/not-found/")
     }
   }
